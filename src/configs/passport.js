e143afd674ed79b20c614a75842fc3be73221cbb
mansurissa <mansurissa6@gmail.com>
2021-06-12 10:52:33 +0200

    correcting on google auth

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -38,30 +38,3 @@ export default (passport) => {
 
   passport.serializeUser((user, done) => done(null, user.id));
 };
-
-// import passport from 'passport';
-// import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-// import { config } from 'dotenv';
-// import { socialAuth } from '../controllers/userController';
-
-// config();
-// passport.serializeUser((user, done) => done(null, user));
-
-// const { CLIENT_ID, CLIENT_SECRET, CB_URL } = process.env;
-
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: CLIENT_ID,
-//       clientSecret: CLIENT_SECRET,
-//       callbackURL: CB_URL,
-//     },
-//     (token, tokenSecret, profile, done) => {
-//       //   User.findOrCreate({ googleId: profile.id }, (err, user) => {
-//       //     return done(err, user);
-//       //   });
-//       socialAuth(profile);
-//       console.log({ profile });
-//     },
-//   ),
-// );
